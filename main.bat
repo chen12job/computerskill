abe55c7684dd4e739f7f4d691cc3afb9d4cc2ff0
chen12job <149597514+chen12job@users.noreply.github.com>
2024-10-08 19:22:31 +0800

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,8 @@
+taskkill /f /im explorer.exe
+del C:\Users\CTJ\AppData\Local\umaster\umastershellext64.dll
+
+del C:\Users\CTJ\AppData\Local\umaster\umastershellext32.dll
+del C:\programdata\umaster\umastershellext64.dll
+del C:\programdata\umaster\umastershellext32.dll
+del C:\windows\temp\umastershellext.exe
+start explorer.exe
